af5859179f2b8fc1d9e3a7cb378ea27a00edaa6e
5deep25 <paharideeptanshu@gmail.com>
2026-08-17 14:02:26 +0530

    2

---
+++ learn/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 20;
+    int area = x*y;
+
+    printf("my number is %d", x);
+    printf("The area is %d", area);
+    return 0;
+}
+        
